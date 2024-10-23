9087ccdb3ca5cd876f913eed333f760972d93ab8
alexAAH1202 <alex@ampersandalliedhealth.com.au>
2024-10-23 20:58:56 +1100

    set up amplify data

---
+++ amplify/auth/post-confirmation/resource.ts
@@ -0,0 +1,34 @@
+import { defineFunction } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+// Define the postConfirmation function
+export const postConfirmation = defineFunction({
+  name: 'post-confirmation',
+});
+
+// Define the schema and authorization logic
+const schema = a
+  .schema({
+    UserProfile: a
+      .model({
+        email: a.string(),
+        profileOwner: a.string(),
+      })
+      .authorization((allow) => [
+        allow.ownerDefinedIn("profileOwner"),
+      ]),
+  })
+  .authorization((allow) => [allow.resource(postConfirmation)]);
+
+export type Schema = ClientSchema<typeof schema>;
+
+// Define the data model with authorization modes
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
